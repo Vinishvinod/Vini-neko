efe6caa94c9186b6dc53ceb1bda2ce04269767df
vinish_luv <90149660+Vinishvinod@users.noreply.github.com>
2024-08-31 17:44:10 +0530

    Update config.js

--- config.js
+++ config.js
@@ -16,9 +16,9 @@ module.exports = {
     /**
      * owner details and parameters
      */
-    ownername: process.env.Owner_Name || "Pika~Kun",
-    ownernumber: process.env.Owner_Number || "916900904828",
-    instagramId: process.env.Insta || "3.69_pika",
+    ownername: process.env.Owner_Name || " vinish_luv ",
+    ownernumber: process.env.Owner_Number || "919035917135",
+    instagramId: process.env.Insta || "vini_neko",
 
     /**
      * other details and parameters
@@ -30,7 +30,7 @@ module.exports = {
     warns: Number(process.env.Warn_Limits) || 3,
     cooldown: Number(process.env.Cooldown_Timer_Seconds) || 5, // default cooldown time per command in seconds
     mongoUrl: process.env.MongoDB || "YOUR_MONGODB_URL",
-    sessionId: process.env.SESSION_ID || "YOUR_SESSION_ID",
+    sessionId: process.env.SESSION_ID || " NWp2bjR_Queen-Anya_odGc= ",
 
     /**
      * thumbnails and medias
